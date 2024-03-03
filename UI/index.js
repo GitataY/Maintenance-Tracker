3f4e41468939d7bdb34e9efbc44944ad2983df98
Wamuyu Gitata <yvonnewamuyug@gmail.com>
2024-03-03 21:19:35 +0300

    user features complete

---
+++ UI/index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('loginButton').addEventListener('click', function() {
+        window.location.href = 'login.html'; // Replace with your login page URL
+    });
+});
